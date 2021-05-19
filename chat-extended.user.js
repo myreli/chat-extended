479994df6b52f9dbecf96f7bca01d11c404a51ae
Myreli <17554234+myreli@users.noreply.github.com>
2021-05-19 17:57:44 -0300

    Create chat-extended.user.js

---
+++ chat-extended.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Chat Extended
+// @version      1.0
+// @description  Improve Google Chat with essential features
+// @author       Myreli <myreli.dev>
+// @grant        none
+// @match        https://chat.google.com/*
+// @icon         https://www.google.com/s2/favicons?domain=chat.google.com
+// @source       https://myreli.dev/tools/chat-extended
+// @require      https://gist.githubusercontent.com/myreli/ede298044156ac1894ca86c1c9bc8747/raw/3937dc2cf28a1f04194b8293f8f7cb940700db03/script.min.js
+// ==/UserScript==
